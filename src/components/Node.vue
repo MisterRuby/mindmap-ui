53ed56c3b4f10248b466605c2b67c305dcd8398f
Ruby <93859705+MisterRuby@users.noreply.github.com>
2024-09-08 21:26:47 +0900

    modify : Node 컴포넌트 좌표 props 추가

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -2,36 +2,41 @@
   <!-- Node Component -->
   <!-- TODO - 텍스트 크기를 변경할 수 있어야 한다. -->
   <!-- TODO - Node 에 대한 크기, 좌표, 텍스트 등 컴포넌트에 대한 내용을 DB 에 저장하여 불러올 때 적용한다. -->
-  <div
-    class="container"
-    :style="{ width: props.width + 'px', height: props.height + 'px' }"
-  >
+  <div class="container" :style="containerStyle">
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      :width="props.width"
-      :height="props.height"
+      :width="width"
+      :height="height"
       :viewBox="viewBox"
     >
       <rect
-        x="0"
-        y="0"
-        :width="props.width"
-        :height="props.height"
-        :rx="props.height / 2"
-        :ry="props.height / 2"
-        :fill="props.fill"
+        :x="x"
+        :y="y"
+        :width="width"
+        :height="height"
+        :rx="height / 2"
+        :ry="height / 2"
+        :fill="fill"
       ></rect>
     </svg>
     <input
       class="text"
       type="text"
-      :value="props.text"
+      :value="text"
       placeholder="Please enter the central topic"
     />
   </div>
 </template>
 <script setup lang="ts">
-const props = defineProps({
+const { x, y, width, height, text, fill } = defineProps({
+  x: {
+    type: Number,
+    default: 100,
+  },
+  y: {
+    type: Number,
+    default: 100,
+  },
   width: {
     type: Number,
     default: 120,
@@ -50,14 +55,24 @@ const props = defineProps({
   },
 });
 
+const containerStyle = computed<object>(() => {
+  return {
+    position: "absolute",
+    left: `${x}px`,
+    top: `${y}px`,
+    width: `${width}px`,
+    height: `${height}px`,
+  };
+});
+
 const viewBox = computed<string>(() => {
-  return `0 0 ${props.width} ${props.height}`;
+  return `${x} ${y} ${width} ${height}`;
 });
 </script>
 
 <style scoped>
 .container {
-  position: relative;
+  position: absolute;
 }
 
 .text {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,41 @@
 <template>
   <!-- <HelloWorld /> -->
-  <Node text="test1" />
-  <Node width="300" height="45" text="test2" fill="dodgerblue" />
+  <div class="fill-height">
+    <Node
+      v-for="node in nodes"
+      :key="node.text"
+      :x="node.x"
+      :y="node.y"
+      :width="node.width"
+      :height="node.height"
+      :text="node.text"
+      :fill="node.fill"
+    ></Node>
+  </div>
 </template>
 
 <script lang="ts" setup>
-//
+const nodes = ref<object>([
+  {
+    x: 0,
+    y: 0,
+    text: "test1",
+  },
+  {
+    x: 100,
+    y: 50,
+    width: 300,
+    height: 45,
+    text: "test2",
+    fill: "dodgerblue",
+  },
+]);
 </script>
+
+<style>
+.fill-height {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+</style>
